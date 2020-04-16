55629452c14578659cc00a26378c26dea52db0c3
arrxi <16142117275@qq.com>
2020-04-16 16:48:09 +0800

    补充了双向链表
    未经过测试，必须需要经过测试才能使用
    未实现foreach的迭代方法

---
+++ CodeRepositoy for CS/CodeRepositoy for CS/DoublyLinkedList.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeRepositoy_for_CS {
+
+    public class DoublyLinkedList<T> : IEnumerable<T> where T : class {
+        public Node<T> head;
+        public Node<T> tail;
+        public int count = 0;
+
+        public DoublyLinkedList()
+        {
+            List<T> t = new List<T>();
+        }
+
+        public void Insert(T t, int index)
+        {
+            if (count > index)
+            {
+                Node<T> tmp = head; int i = 0;
+                while (true)
+                {
+                    tmp = tmp.child;
+                    if (i == index)
+                    {
+                        Node<T> newNode = new Node<T>() { data = t };
+
+                        newNode.parent = tmp.parent;
+                        newNode.child = tmp.child;
+
+                        newNode.parent.child = newNode;
+                        newNode.child.parent = newNode;
+                        break;
+                    }
+                    i++;
+                }
+                count++;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+                //Add(t);
+            }
+        }
+
+        public void InsertAfterNode(Node<T> node, T t)
+        {
+            Node<T> inser = new Node<T>() { data = t };
+            bool isend = node.IsEnd;
+            node.child = inser;
+            inser.parent = node;
+            if (!isend)
+            {
+                node.child.parent = inser;
+                inser.child = node.child;
+            }
+            else
+            {
+                tail = inser;
+            }
+
+            count++;
+        }
+
+        public void InsertBeforeNode(Node<T> node, T t)
+        {
+            Node<T> inser = new Node<T>() { data = t };
+            bool isstart = node.IsStart;
+            inser.child = node.child;
+            node.child.parent = inser;
+            if (!isstart)
+            {
+                node.parent.child = inser;
+                inser.parent = node;
+            }
+            else
+            {
+                head = inser;
+            }
+
+            count++;
+        }
+
+        /// <summary>
+        /// 在链表尾端添加元素
+        /// </summary>
+        /// <param name="t"></param>
+        public void Add(T t)
+        {
+            if (head == null)
+            {
+                head = new Node<T>() { data = t };
+                tail = head;
+            }
+            else
+            {
+                var tmp = new Node<T>() { data = t };
+                tmp.parent = tail;
+            }
+            count++;
+        }
+
+        /// <summary>
+        /// 删除尾节点
+        /// </summary>
+        public void Remove()
+        {
+            if (count >= 1)
+            {
+                tail = tail.parent;
+                tail.child = null;
+                count--;
+            }
+            else
+            {
+                Console.WriteLine("列表数据为空");
+            }
+        }
+
+        public void Remove(Node<T> node)
+        {
+            if (node.IsStart)
+            {
+                node.child.parent = null;
+                head = node.child;
+            }
+            else if (node.IsEnd)
+            {
+                node.parent.child = null;
+                tail = node.parent;
+            }
+            else
+            {
+                node.parent.child = node.child;
+                node.child.parent = node.parent;
+            }
+            count--;
+        }
+
+        /// <summary>
+        /// 返回并删除尾节点 ，为空时候返回空
+        /// </summary>
+        /// <returns></returns>
+        public T Pop()
+        {
+            if (count >= 1)
+            {
+                var tmp = tail.data;
+                tail = tail.parent;
+                tail.child = null;
+                count--;
+                return tmp;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 返回并删除头节点
+        /// </summary>
+        /// <returns></returns>
+        public T PopOnHead()
+        {
+            if (count <= 0)
+            {
+                return null;
+            }
+            else
+            {
+                var tmp = head;
+                head = tail;
+                if (head != null)
+                {
+                    head.parent = null;
+                }
+                count--;
+                return tmp.data;
+            }
+        }
+
+        /// <summary>
+        ///删除固定索引的元素
+        /// </summary>
+        /// <param name="index"></param>
+        public void RemoveAt(int index)
+        {
+            if (count > index)
+            {
+                Node<T> tmp = head; int i = 0;
+                while (true)
+                {
+                    tmp = tmp.child;
+                    if (i == index)
+                    {
+                        tmp.parent.child = tmp.child;
+                        tmp.child.parent = tmp.parent;
+                        tmp.child = null;
+                        tmp.parent = null;
+                        count--;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Node<T> {
+        public bool IsStart { get => parent == null ? true : false; }
+
+        public bool IsEnd { get => child == null ? true : false; }
+        public Node<T> parent;
+        public Node<T> child;
+        public T data;
+
+        public Node<T> Next()
+        {
+            return child;
+        }
+
+        public Node<T> Last()
+        {
+            return parent;
+        }
+    }
+}
--- CodeRepositoy for CS/CodeRepositoy for CS/Program.cs
+++ CodeRepositoy for CS/CodeRepositoy for CS/Program.cs
@@ -4,20 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeRepositoy_for_CS
-{
+namespace CodeRepositoy_for_CS {
 
     public class Program {
 
         public static void Main(String[] args)
         {
             //SortTest();
-            Console.WriteLine("hle".Mul(4));
+            //Console.WriteLine("hle".Mul(4));
+            TestLinkedListD();
             Console.ReadLine();
-
-
-
         }
+
+        public static void TestLinkedListD()
+        {
+            DoublyLinkedList<Ixl> linklist = new DoublyLinkedList<Ixl>();
+            for (int i = 0; i < 10; i++)
+            {
+                linklist.Add(new Ixl());
+            }
+            Console.WriteLine(linklist.count);
+        }
+
         public static void SortTest()
         {
             Random rd = new Random();
@@ -25,7 +33,7 @@ namespace CodeRepositoy_for_CS
             for (int i = 0; i < 50; i++)
             {
                 list.Add(rd.Next(100));
-                Console.Write(list[i]+"  ");
+                Console.Write(list[i] + "  ");
             }
             Console.WriteLine();
             //SortHelper.InsertSort(list, 0, list.Count - 1, (a, b) => { return a - b; });
@@ -33,9 +41,18 @@ namespace CodeRepositoy_for_CS
             SortHelper.ShellSort(list, (a, b) => { return a - b; });
             for (int i = 0; i < list.Count; i++)
             {
-                Console.Write(list[i]+"  ");
+                Console.Write(list[i] + "  ");
             }
+        }
+    }
 
+    internal class Ixl {
+        private static Random rd = new Random();
+        public int t;
+
+        public Ixl()
+        {
+            t = rd.Next(100);
         }
     }
 }
